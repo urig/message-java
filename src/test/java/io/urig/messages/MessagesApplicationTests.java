187e00f5febab737cd24e47a512a9bf4fe104573
Uri Goldstein <uri.goldstein@gmail.com>
2021-03-30 12:50:43 +0300

    An integ test for GET

--- src/test/java/io/urig/messages/MessagesApplicationTests.java
+++ src/test/java/io/urig/messages/MessagesApplicationTests.java
@@ -1,13 +1,29 @@
 package io.urig.messages;
 
+import jdk.jfr.Category;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+@Category("Integration")
 class MessagesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@LocalServerPort
+	private int port;
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void GET_newRecipient_emptyList() {
+		String actual = this.restTemplate
+				.getForObject("http://localhost:" + port + "/messages/foo", String.class);
+		assertThat(actual).isEqualTo("[]");
+	}
 }
